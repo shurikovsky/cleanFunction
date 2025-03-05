ab14f6d3c662190d6dcd26bf5b9b456b5f2063bf
Alexander Kryukov 6522320 <AKryukov5@slb.ru>
2025-03-05 11:42:50 +0500

    jest error

---
+++ test/lifeIndicator.js
@@ -0,0 +1,19 @@
+import lifeIndicator from '..lifeIndicator.js';
+test.each([
+    [
+        'critical', 10
+    ],
+    [
+        'wounded', 45
+    ],
+    [
+        'healthy', 75
+    ],
+])(
+    ('indicator %c for life level %l'),
+    (color, level) => {
+        const result = lifeIndicator({
+            name: 'Маг', health: level
+        });
+        expect(result).toBe(color);
+    });
